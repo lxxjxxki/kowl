3eb6924966ea32cd046c57f3824ec0a246988006
lxxjxxki <teddyjk1@naver.com>
2024-06-04 03:42:18 +0900

    fix #7: html, css, js 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -6,7 +6,6 @@ const hiddenArchiveLink = document.getElementById('hiddenArchiveLink');
 const hiddenInstagramLogoLink = document.getElementById('hiddenInstagramLogoLink');
 const hiddenXImage = document.getElementById('hiddenXImage');
 
-
 const MenuIconElements = { menuIcon, hiddenMenuRectangle, hiddenMenuIcon, hiddenShopLink, hiddenArchiveLink, hiddenInstagramLogoLink, hiddenXImage };
 
 
@@ -23,3 +22,25 @@ hiddenXImage.addEventListener("click", function(){
     });
     menuIcon.style.display = "block";
 });
+
+function getCartItemCount() {
+    // 장바구니에 담긴 상품의 개수 계산
+    var count = 0; // 0을 무조건 바꿔줘야함(JAVA - AJAX[yml])
+    
+    
+    return count;
+}
+
+// 장바구니에 담긴 상품에 따라 표시되는 HTML 요소 선택
+const cartNoInformation = document.getElementById('cartNoInformation');
+const cartYesInformation = document.getElementById('cartYesInformation');
+
+// 장바구니에 담긴 상품의 개수를 가져와서 조건부 렌더링
+if (getCartItemCount() === 0) {
+  cartNoInformation.style.display = 'block';
+  cartYesInformation.style.display = 'none';
+} else {
+  cartNoInformation.style.display = 'none';
+  cartYesInformation.style.display = 'block';
+}
+
